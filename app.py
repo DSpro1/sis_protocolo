219d707e59b128e97ddd0c09444fb80f15a115b3
DSpro1 <danielverissimo1d@gmail.com>
2023-12-01 12:36:14 -0300

    cadastro de usuario, login, + validações

--- app.py
+++ app.py
@@ -28,19 +28,10 @@ class Protocolo(db.Model):
         self.assunto=assunto
         self.data=data
 
-
-usuario=Administrador("Daniel", "123")
-usuario2=Administrador("Marcelo", "147")
-protocolo=Protocolo("licença prêmio", "27/11/2023")
-
 with app.app_context():
     db.drop_all()
     db.create_all()
-    db.session.add(usuario)
-    db.session.add(usuario2)
-    db.session.add(protocolo)
-    db.session.commit()
-
+  
 #=======autenticação=======
 from flask_wtf import FlaskForm
 from wtforms import StringField
@@ -55,46 +46,65 @@ class FormLogin(FlaskForm):
 
 class FormProtocolo(FlaskForm):
     assunto = StringField("assunto", validators=[input_required()])
-    data = StringField('data', validators=[input_required()])
+    data = StringField("data", validators=[input_required()])
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
     formLogin = FormLogin()
-
-    if formLogin.validate_on_submit():
+    if formLogin.validate_on_submit(): #testado
+        
         nomeUsuario = formLogin.nomeUsuario.data
         senha = formLogin.senha.data
+               
         return autenticacao(nomeUsuario, senha)
-    return render_template('index.html', form = formLogin)
-
+    return render_template('index.html', form = formLogin, login=True)
 
 def autenticacao(nomeUsuario, senha):
     users=recuperaUsuarios()
-    print(users)
-    print(nomeUsuario in users )
     if nomeUsuario in users and users[nomeUsuario]==senha:
         return redirect(url_for('visual_protocolo'))
     else:
         return f'usuário ou senha inválida, tente novamente' 
-    
+
 def recuperaUsuarios():
     users = db.session.query(Administrador).all() 
     users_dict={}
-    print(users)
     for user in users:
         users_dict[user.nomeUsuario]=user.senha
     return users_dict
-  
+
+@app.route("/cadastra_usuario", methods=['GET', 'POST'])
+def cadastraUsuario():
+    formCadastro = FormLogin()
+    if formCadastro.validate_on_submit():
+        nome = formCadastro.nomeUsuario.data
+        senha=formCadastro.senha.data
+        adm = Administrador(nome, senha)
+        db.session.add(adm)
+        db.session.commit()
+    return render_template('index.html', form=formCadastro)
+#==========protocolos=========
 @app.route('/visual_protocolo')
 def visual_protocolo():
     protocolos = db.session.query(Protocolo).all()
-   
     protocoloDict={}
     for protocolo in protocolos:
-    
         protocoloDict[protocolo.numero]={"assunto":protocolo.assunto, "data":protocolo.data}
-
     return jsonify(protocoloDict)
+
+@app.route('/cad_protocolo', methods=['GET', 'POST'])
+def cad_protocolo():
+    form_protocolo = FormProtocolo()
+    
+    if form_protocolo.validate_on_submit():
+        assunto = form_protocolo.assunto.data
+        data =  form_protocolo.data.data
+        protocolo = Protocolo(assunto, data)
+        db.session.add(protocolo)
+        db.session.commit()
+        return "cadastrado com sucesso"
+        
+    return render_template('/cadastraProtocolo.html', formProtoc = form_protocolo)
 
 if __name__ == "__main__":
     app.run(debug=True)
